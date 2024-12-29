fabcb7ce00f7cb01b5b9b2a3698c1ff81cb7460c
zeinab-alhwary <zeinabnebeshi20@gmail.com>
2024-12-29 17:29:43 +0200

    adding some fixed on responsive style

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -13,25 +13,25 @@ export class OverviewComponent {
 
 overviewData=[{
   voucher:true,
-  image:'../../../assets/images/voucher 1.svg',
+  image:'assets/images/voucher 1.svg',
   text:'open tickets',
   number:'270',
 },
 {
   rotate:true,
-  image:'../../../assets/images/rotate-reverse_16497904 1.svg',
+  image:'assets/images/rotate-reverse_16497904 1.svg',
   text:'In progress tickets',
   number:'136',
 },
 {
   hourglass:true,
-  image:'../../../assets/images/hourglass-sand-bottom 1.svg',
+  image:'assets/images/hourglass-sand-bottom 1.svg',
   text:'Pending tickets',
   number:'346',
 },
 {
   doubleTick:true,
-  image:'../../../assets/images/double-tick 1.svg',
+  image:'assets/images/double-tick 1.svg',
   text:'Closed tickets',
   number:'150',
 }
